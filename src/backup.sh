df64786eb0c28d95fcf7e68a6c67a6691dcd449c
Harry Webster <harry@diffordsguide.com>
2018-03-21 02:04:12 +0000

    Started to add encryption/decryption code to the backup process... we will also need a means to decrypt the data for the user to view at a later stage.

--- src/backup.sh
+++ src/backup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Detect if the backup store is encrypted
+if [ -f /backup/destination/encrypted ]; then
+	// decrypt destination here...
+fi
+
 echo `date '+%Y-%m-%d %H:%M:%S'` : Starting to mirror contents of source
 echo
 rsync -avzu --delete --stats /backup/source /backup/destination/latest/
@@ -15,3 +20,13 @@ echo
 rsnapshot -c /backup/rsnapshot.conf du
 echo
 echo `date '+%Y-%m-%d %H:%M:%S'` : Finished backup
+
+# If the encryption key is found then encrypt the backup location before
+# finishing up.
+if [ -f /backup/private.key ]; then
+	// encrypt destination here...
+	tar -cf /backup/destination/encrypted-latest.tar /backup/destination/latest
+	tar -cf /backup/destination/encrypted-snapshots.tar /backup/destination/snapshots
+	rm -rf /backup/destination/latest
+	rm -rf /backup/destination/snapshots
+fi
